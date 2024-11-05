06182ebff11781f9dcf38f7f0c86374eb9a1249c
jhudielden <138587752+jhudielden@users.noreply.github.com>
2024-11-05 18:28:17 +0000

    update

--- Assets/Scripts/TilesScript.cs
+++ Assets/Scripts/TilesScript.cs
@@ -5,12 +5,24 @@ using UnityEngine;
 public class TilesScript : MonoBehaviour
 {
     public Vector3 targetPosition;
+    private Vector3 correctPosition;
+    private SpriteRenderer _sprite;
     void Start()
     {
         targetPosition = transform.position;
+        correctPosition = transform.position;
+        _sprite = GetComponent<SpriteRenderer>();
     }
     void Update()
     {
         transform.position = Vector3.Lerp(transform.position, targetPosition, 0.05f);
+        if (targetPosition == correctPosition)
+        {
+            _sprite.color = Color.grey;
+        }
+        else
+        {
+            _sprite.color = Color.white;
+        }
     }
 }
