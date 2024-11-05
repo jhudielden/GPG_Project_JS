e09353731e1e4f93af2a50e6a32ca6aab93cace6
jhudielden <138587752+jhudielden@users.noreply.github.com>
2024-11-05 13:24:47 +0000

    Initial commit

---
+++ Assets/Scripts/GameScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScript : MonoBehaviour
+{
+    [SerializeField] private Transform emptySpace = null;
+    private Camera _camera;
+    void Start()
+    {
+        _camera = Camera.main;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+            if (hit)
+            {
+                if (Vector2.Distance(emptySpace.position, hit.transform.position) < 1.4f)
+                {
+                    Vector2 lastEmptySpacePosition = emptySpace.position;
+                    emptySpace.position = hit.transform.position;
+                    hit.transform.position = lastEmptySpacePosition;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float Hinput = Input.GetAxis("Horizontal");
+        float Vinput = Input.GetAxis("Vertical");
+        transform.Translate(Vector2.right * speed * Hinput * Time.deltaTime);
+        transform.Translate(Vector2.up * speed * Vinput * Time.deltaTime);
+
+    }
+}
